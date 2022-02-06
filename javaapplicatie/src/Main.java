3f3320ceca9ca4b6dc24e35e068faae1afc71573
Maurice Snoeren <maurice.snoeren@avans.nl>
2022-02-06 11:46:57 +0100

    Added method sum and textual user interface

--- javaapplicatie/src/Main.java
+++ javaapplicatie/src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	 // write your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please give the first value a: ");
+        int a = Integer.parseInt( scanner.nextLine() );
+
+        System.out.print("Please give the first value b: ");
+        int b = Integer.parseInt( scanner.nextLine() );
+
+        System.out.println(a + " + " + b + " = " + sum(a, b));
+    }
+
+    static int sum( int a, int b ) {
+        int sum = a + b;
+        return sum;
     }
 }
