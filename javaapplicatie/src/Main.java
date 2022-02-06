80f67c0318db9ae452da04c2899b1326209e5d1a
Maurice Snoeren <maurice.snoeren@avans.nl>
2022-02-06 11:50:27 +0100

    Added comments and refactored textual userinterface, added gitignore for this project

--- javaapplicatie/src/Main.java
+++ javaapplicatie/src/Main.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 
 public class Main {
 
+    // Main method that is invoked when the application is started
     public static void main(String[] args) {
+        startUserInterface();
+    }
 
+    // This method implements the user interface of the application.
+    static void startUserInterface() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Please give the first value a: ");
@@ -15,6 +20,7 @@ public class Main {
         System.out.println(a + " + " + b + " = " + sum(a, b));
     }
 
+    // Method returns the interger sum of two integers.
     static int sum( int a, int b ) {
         int sum = a + b;
         return sum;
